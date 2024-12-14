7afbd5e8a372afe0fcd083cc5af1d4125fa450a2
Jan Joshua De Guzman <Jan Joshua De Guzman>
2024-12-14 14:55:59 +0800

    Ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
